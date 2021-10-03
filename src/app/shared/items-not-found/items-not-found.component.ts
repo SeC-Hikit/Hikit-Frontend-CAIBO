ee584eb61a78845cf7a0c05f0b4dfb4c5c8874c7
Lorenzo Verri <lore.verri@gmail.com>
2021-10-03 23:44:35 +0200

    Fixed maintenance management page

--- src/app/shared/items-not-found/items-not-found.component.ts
+++ src/app/shared/items-not-found/items-not-found.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-items-not-found',
-  templateUrl: './items-not-found.component.html',
-  styleUrls: ['./items-not-found.component.scss']
+  selector: "app-items-not-found",
+  templateUrl: "./items-not-found.component.html",
+  styleUrls: ["./items-not-found.component.scss"],
 })
 export class ItemsNotFoundComponent implements OnInit {
+  @Input() text?: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
