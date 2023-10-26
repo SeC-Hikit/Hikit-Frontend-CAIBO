5756efdd07ef4f927be0f7b0bedf18ce6709fc2d
Lorenzo Verri <lore.verri@gmail.com>
2023-10-26 09:53:53 +0200

    Updated bounds

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,8 +9,8 @@ export const environment = {
     realm: "cai-bologna",
     instance: "sec_bologna_1",
     publicName: "CAI Bologna",
-    boundsTopLeft: [44.73, 10.62],
-    boundsBottomRight: [44.091777, 12.073912],
+    boundsTopLeft: [44.810096, 10.355102],
+    boundsBottomRight: [43.741833, 12.036322],
     issueReportingList: [
         "Schianto d'albero su sentiero",
         "Sentiero smottato/franato",
