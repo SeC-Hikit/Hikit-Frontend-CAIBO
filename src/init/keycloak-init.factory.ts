e9c5a53e60450b631bceb99bb3adc030781401a5
Lorenzo Verri <lore.verri@gmail.com>
2023-11-26 22:09:49 +0100

    #39 Added un minuto di rumore

--- src/init/keycloak-init.factory.ts
+++ src/init/keycloak-init.factory.ts
@@ -1,16 +1,16 @@
 import {KeycloakService} from "keycloak-angular";
 
 export function initializeKeycloak(keycloak: KeycloakService) {
-  return () => {}
-    // keycloak.init({
-    //   config: {
-    //     url: "https://kc.sentieriecartografia.it/auth",
-    //     realm: "SeC-Test",
-    //     clientId: "backend",
-    //   },
-    //   initOptions: {
-    //     enableLogging: true,
-    //     onLoad:"check-sso"
-    //   },
-    // });
+  return () =>
+    keycloak.init({
+      config: {
+        url: "https://kc.sentieriecartografia.it/auth",
+        realm: "SeC-Test",
+        clientId: "backend",
+      },
+      initOptions: {
+        enableLogging: true,
+        onLoad:"check-sso"
+      },
+    });
 }
