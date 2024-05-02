9d395b2d6674c9ec876c77205e4e3ce2afb65b1c
Lorenzo Verri <lore.verri@gmail.com>
2024-05-02 16:32:30 +0200

    #275 Add 'altro' to possible reports

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,24 +1,25 @@
 export const environment = {
-  production: true,
+    production: true,
 
-  // S&C Env vars
-  realm: "cai-bologna",
-  instance: "sec_bologna_1",
-  publicName: "CAI Bologna",
-  northWestBoundsLatLng: [44.810096, 10.355102],
-  southEastBoundsLatLng: [43.741833, 12.036322],
-  issueReportingList: [
-    "Albero/i caduti",
-    "Sentiero smottato/franato",
-    "Sentiero invaso da piante",
-    "Sentiero non visibile",
-    "Segnaletica mancante",
-    "Cartelli direzionali rotti",
-    "Allagamento",
-    "Frana di sassi da monte",
-    "Frana di terra da monte",
-    "Fune di sicurezza danneggiata",
-    "Gradini danneggiati",
-  ]
+    // S&C Env vars
+    realm: "cai-bologna",
+    instance: "sec_bologna_1",
+    publicName: "CAI Bologna",
+    northWestBoundsLatLng: [44.810096, 10.355102],
+    southEastBoundsLatLng: [43.741833, 12.036322],
+    issueReportingList: [
+        "Albero/i caduti",
+        "Sentiero smottato/franato",
+        "Sentiero invaso da piante",
+        "Sentiero non visibile",
+        "Segnaletica mancante",
+        "Cartelli direzionali rotti",
+        "Allagamento",
+        "Frana di sassi da monte",
+        "Frana di terra da monte",
+        "Fune di sicurezza danneggiata",
+        "Gradini danneggiati",
+        "Altro"
+    ]
 
 };
